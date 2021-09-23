bdb733bfb346888954041e420eae6b2292ecbb8e
Jason Le <leminhtam.rp@gmail.com>
2021-09-23 09:19:27 +0800

    add restsharp, specflow

--- Liberia_E2E/Liberia_E2E/UnitTest1.cs
+++ Liberia_E2E/Liberia_E2E/UnitTest1.cs
@@ -1,4 +1,4 @@
-using System;
+using RestSharp;
 using Xunit;
 
 namespace Liberia_E2E
@@ -8,6 +8,12 @@ namespace Liberia_E2E
         [Fact]
         public void Test1()
         {
+            var client = new RestClient("http://localhost:3000/");
+
+            var request = new RestRequest("posts/{postid}",Method.GET);
+            request.AddUrlSegment("postid", 1);
+
+            var content = client.Execute(request).Content;
 
         }
     }
